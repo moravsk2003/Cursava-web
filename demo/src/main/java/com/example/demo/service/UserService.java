57567beca72284a45909ea0697bb2a344b78f653
moravsk2003 <126240228+moravsk2003@users.noreply.github.com>
2025-04-30 12:06:55 +0300

    додав можливість очишчати базу данних
    новий метод clearUserTable

--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -5,7 +5,9 @@ import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -16,11 +18,22 @@ import java.util.Optional;
 //@RequiredArgsConstructor
 @Service
 public class UserService {
+    @Autowired
     private  final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
 
         this.userRepository = userRepository;
+    }
+    @Transactional // Видалення має виконуватися в транзакції
+    public void clearYourTable() {
+        // Варіант 1: deleteAll() - може бути повільнішим для великих таблиць, видаляє сутності по черзі
+        // yourEntityRepository.deleteAll();
+
+        // Варіант 2: deleteAllInBatch() - зазвичай швидше, виконує один SQL DELETE запит
+        userRepository.deleteAllInBatch();
+
+        System.out.println("Таблиця YourEntity очищена.");
     }
     public List<User> getAllUsers(){
 
