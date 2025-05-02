115845092440ce8ae9862fa03a229b36797f4a73
moravsk2003 <126240228+moravsk2003@users.noreply.github.com>
2025-05-02 13:39:50 +0300

    fix lombok

--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -10,8 +10,6 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Getter
-@Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class User {
     @Id
@@ -42,58 +40,4 @@ public class User {
 
     }
 
-    public User() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 }
-
-
-
