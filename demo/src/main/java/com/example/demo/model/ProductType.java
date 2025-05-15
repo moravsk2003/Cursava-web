ed5f58d468a70fe24b32edf1987805d589dc2c30
moravsk2003 <kfcrfdjghjcbvj@gmail.com>
2025-05-15 17:26:23 +0300

    5

--- demo/src/main/java/com/example/demo/model/ProductType.java
+++ demo/src/main/java/com/example/demo/model/ProductType.java
@@ -2,13 +2,7 @@ package com.example.demo.model;
 
 public enum ProductType {
     ANIME,
-    Book,
-    Film,
     GAME,
-    Nowela,
-    Manga,
-    Manhva,
-    Manhvua,
     BOOK,
     MANHVA,
     FILM,
